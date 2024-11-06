84c179db6412bb51d870d6d92bf85f748128a6c9
DevmanYur <devmanyur@gmail.com>
2024-11-06 21:38:30 +0300

    Закрыл блок with open(...) as file

--- quiz.py
+++ quiz.py
@@ -7,18 +7,18 @@ def get_quiz():
     file_with_quiz_questions = random.choice(files_with_quiz_questions)
     with open(f'{folder_for_quiz_questions}/{file_with_quiz_questions}', "r", encoding="KOI8-R") as my_file:
         file_contents = my_file.read()
-        units = file_contents.split('\n\n\n')
-        quiz = []
-        for unit in units:
-            parts = unit.split('\n\n')
-            for part in parts:
-                if "Вопрос " in part:
-                    index_symbol = part.find(':')+2
-                    question = part[index_symbol:]
-                if "Ответ:" in part:
-                    index_symbol = part.find(':')+2
-                    answer = part[index_symbol:]
-            part_of_quiz = {'Вопрос': question,
-                            'Ответ': answer}
-            quiz.append(part_of_quiz)
-        return quiz
+    units = file_contents.split('\n\n\n')
+    quiz = []
+    for unit in units:
+        parts = unit.split('\n\n')
+        for part in parts:
+            if "Вопрос " in part:
+                index_symbol = part.find(':')+2
+                question = part[index_symbol:]
+            if "Ответ:" in part:
+                index_symbol = part.find(':')+2
+                answer = part[index_symbol:]
+        part_of_quiz = {'Вопрос': question,
+                        'Ответ': answer}
+        quiz.append(part_of_quiz)
+    return quiz
